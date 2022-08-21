15be5ac4c75dd018ccef860e194c32745e8f93b8
huzaifa003 <buntyhuzaifa3@gmail.com>
2022-08-21 13:35:02 +0500

    Fixed Icon color

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -7,47 +7,47 @@ import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
 
 
-const Main = ({darkMode,setDarkMode}) => {
+const Main = ({ darkMode, setDarkMode }) => {
 
   return (
     <div>
-       <section className="main-section">
+      <section className="main-section">
         <div className="container">
-        
-            <img className='h-[40%] w-[40%]' src={Logo} alt=""/>
-          
-            <p>Welcome to the official cccambox proxy, if our website main url is not
-                available in google you can then use one of the links below to access
-                our services. Enjoy 🎉
-            </p>
-         
 
-           
-            <div  className="boxes ">
-          <a style={{display:'contents'}} href="http://google.com">
-                <div  className={`${darkMode ? 'box' : 'boxDark'}   `}>
-                    <WebIcon style={{width:"50%"}}></WebIcon>
-               <h6 className="box-text ">Website</h6>
-                </div>
-                </a>
-                
-                <a style={{display:'contents'}} href="http://google.com">
-                <div  className={`${darkMode ? 'box' : 'boxDark'}   `}>
-                <PeopleAltIcon style={{width:"50%"}}></PeopleAltIcon>
-                    <h6  className="box-text ">Members Area</h6>
-                </div>
-                </a>
+          <img className='h-[40%] w-[40%]' src={Logo} alt="" />
 
-                <a style={{display:'contents'}} href="http://google.com">
-                <div  className={`${darkMode ? 'box' : 'boxDark'}   `}>
-               <ShoppingCartCheckoutIcon style={{width:"50%"}} ></ShoppingCartCheckoutIcon>
-                   
-                    <h6 className="box-text ">Checkout</h6>
-                </div>
-                </a>
-            </div>
+          <p>Welcome to the official cccambox proxy, if our website main url is not
+            available in google you can then use one of the links below to access
+            our services. Enjoy 🎉
+          </p>
+
+
+
+          <div className="boxes ">
+            <a style={{ display: 'contents' }} href="http://google.com">
+              <div className={`${darkMode ? 'box' : 'boxDark'}   `}>
+                <WebIcon style={{ width: "50%" }} ></WebIcon>
+                <h6 className={`${darkMode ? 'boxTextDark' : 'boxTextLight'}   `}> Website </h6>
+              </div>
+            </a>
+
+            <a style={{ display: 'contents' }} href="http://google.com">
+              <div className={`${darkMode ? 'box' : 'boxDark'}   `}>
+                <PeopleAltIcon style={{ width: "50%" }}></PeopleAltIcon>
+                <h6 className={`${darkMode ? 'boxTextDark' : 'boxTextLight'}   `}> Members Area </h6>
+              </div>
+            </a>
+
+            <a style={{ display: 'contents' }} href="http://google.com">
+              <div className={`${darkMode ? 'box' : 'boxDark'}   `}>
+                <ShoppingCartCheckoutIcon style={{ width: "50%" }} ></ShoppingCartCheckoutIcon>
+
+                <h6  className={`${darkMode ? 'boxTextDark' : 'boxTextLight'}   `}> Checkout </h6>
+              </div>
+            </a>
+          </div>
         </div>
-    </section>
+      </section>
     </div>
   )
 }
